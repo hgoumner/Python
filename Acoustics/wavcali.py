43a472f98c92f5d105c575148c0c361c228a4d67
Hristo <goumnerov@gmail.com>
2018-06-24 17:06:18 +0200

    update py file to match notebook

--- Acoustics/wavcali.py
+++ Acoustics/wavcali.py
@@ -35,38 +35,45 @@ tt = 10
 x = np.arange(0, tt, n)
 
 # signal frequency
-sigf = 10
+sigf = 5
 
 # signal 1
 f1 = 2*np.pi*sigf
 ph1 = 0
-y1 = np.sin(f1*x - ph1*sf*n)
+ns1 = np.random.rand(len(x))/10
+y1 = np.sin(f1*x - ph1*sf*n) + ns1
 
 # signal 2
 f2 = 2*np.pi*sigf
 ph2 = 1/2*np.pi
-y2 = np.sin(f2*x - ph2*sf*n)
+ns2 = np.random.rand(len(x))/10
+y2 = np.sin(f2*x - ph2*sf*n) + ns2
 
 # signal 3
 f3 = 2*np.pi*sigf
 ph3 = np.pi
-y3 = np.sin(f3*x - ph3*sf*n)
+ns3 = np.random.rand(len(x))/10
+y3 = np.sin(f3*x - ph3*sf*n) + ns3
 
 # signal 4
 f4 = 2*np.pi*sigf
 ph4 = 3/2*np.pi
-y4 = np.sin(f4*x - ph4*sf*n)
+ns4 = np.random.rand(len(x))/10
+y4 = np.sin(f4*x - ph4*sf*n) + ns4
 
 #%% Plot signals
 
-fig = plt.figure()
+fig = plt.figure(figsize=(16,6))
+
+# plotting increment for faster computation
 npl = 100
-plt.plot(x[::npl], y1[::npl], x[::npl], y2[::npl], x[::npl], y3[::npl], x[::npl], y4[::npl])
+plt.plot(x[::npl], y1[::npl], x[::npl], y2[::npl], x[::npl], y3[::npl], \
+         x[::npl], y4[::npl])
 plt.xlim([0, tt/10])
 plt.grid()
 plt.xlabel('Time')
 plt.ylabel('Magnitude')
-plt.legend(['1', '2', '3', '4'])
+plt.legend(['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4'], loc=3)
 
 #%% Export data
 
@@ -75,13 +82,15 @@ out = np.c_[y1, y2, y3, y4]
 out = np.asarray(out, dtype=np.float32)
 
 # filename
-fn = r'R:\166\TRANSFER_all\Goumnerov\cali_t_%d_f1_%d_ph1_%.3f_f2_%d_ph2_%.3f_f3_%d_ph3_%.3f_f4_%d_ph4_%.3f' % (tt, f1, ph1, f2, ph2, f3, ph3, f4, ph4)
+fn = 'cali_t_%d_f1_%d_ph1_%.3f_f2_%d_ph2_%.3f_f3_%d_ph3_%.3f_f4_%d_ph4_%.3f' \
+% (tt, f1, ph1, f2, ph2, f3, ph3, f4, ph4)
 
 # save WAV file
-sp.write(fn + '.wav', sf, out)
+#sp.write(fn + '.wav', sf, out)
 
 # save image
-fig.savefig(fn + '.png')
+#fig.savefig(fn + '.png')
 
 # save CSV file
-np.savetxt(fn + '.csv', out, fmt='%.4e', delimiter=',', header='"SEP=,"', comments='')
+#np.savetxt(fn + '.csv', out, fmt='%.4e', delimiter=',', header='"SEP=,"', \
+#comments='')
