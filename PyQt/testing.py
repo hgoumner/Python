60ea2b44104a66c60a3013abfbd621a6d9b274a6
Hristo Goumnerov <Hristo.Goumnerov@grs.de>
2020-12-26 16:58:34 +0100

    self updating data sample

--- PyQt/testing.py
+++ PyQt/testing.py
@@ -1,4 +1,6 @@
-from PyQt5 import QtWidgets, QtGui
+from random import randint
+
+from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtCore import Qt
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
@@ -10,23 +12,32 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        color = self.palette().color(QtGui.QPalette.Window)
-        styles = {'color': 'r', 'font-size': '20px'}
-
         self.graphWidget = pg.PlotWidget()
-        hour = range(1,11)
-        temperature = [30,32,34,32,33,31,29,32,35,45]
-        pen = pg.mkPen(color='r', width=5, style=Qt.DashLine)
-        self.graphWidget.setBackground(color)
-        self.graphWidget.showGrid(x=True, y=True)
-        self.graphWidget.setTitle('My Title', **styles)
-        self.graphWidget.setLabel('bottom', 'Hour', **styles)
-        self.graphWidget.setLabel('left', 'Temperature', **styles)
-        self.graphWidget.addLegend()
-        self.graphWidget.plot(hour, temperature, name='Test', pen=pen, symbol='+', symbolSize=15, symbolBrush=('b'))
+        self.setCentralWidget(self.graphWidget)
+
+        self.x = [0] #list(range(100))  # 100 time points
+        self.y = [0] #[randint(0,100) for _ in range(100)]  # 100 data points
+
+        self.graphWidget.setBackground('w')
+
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.data_line =  self.graphWidget.plot(self.x, self.y, pen=pen)
 
         self.setCentralWidget(self.graphWidget)
 
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(20)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+
+    def update_plot_data(self):
+        # self.x = self.x[1:]  # Remove the first y element.
+        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+
+        # self.y = self.y[1:]  # Remove the first
+        self.y.append(randint(0, 1))  # Add a new random value.
+
+        self.data_line.setData(self.x, self.y)
 
 def main():
     app = QtWidgets.QApplication([])
